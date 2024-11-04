7be21b225e4c27e552ed21b430143ca8deeec5b7
ansa <ansa@node-0.ansa-227881.ragger-pg0.wisc.cloudlab.us>
2024-11-04 14:44:34 -0600

    add additional perf stats

--- scripts/perf-measure.sh
+++ scripts/perf-measure.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 timestamp=$(date +"%Y%m%d_%H%M%S")
-output_dir="output_$timestamp"
+output_dir="perfstat_output_$timestamp"
 mkdir "$output_dir"
 cd "$output_dir"
 echo "Writing results to $output_dir"
@@ -9,6 +9,6 @@ docker ps --no-trunc | tail -n +2 | while read -r line; do
 	process_id=$(echo $line | awk '{print $1}')
 	name=$(echo $line | awk '{print $3}' | tr -cd '[:alnum:]')
 	echo "recording for service $name"
-	sudo perf stat -e cycles:u,cycles:k,instructions:u,instructions:k,iTLB-load-misses,dTLB-load-misses,branch-misses --cgroup=system.slice/docker-"$process_id".scope -a sleep 60 > "$name".data
+	sudo perf stat -e cycles:u,cycles:k,instructions:u,instructions:k,resource_stalls.any,mem_load_retired.l1_miss,mem_load_retired.l2_miss,itlb_misses.stlb_hit,itlb_misses.miss_causes_a_walk,iTLB-load-misses,dTLB-load-misses,branch-misses --cgroup=system.slice/docker-"$process_id".scope -a sleep 60 > "$name".data
 done
 cd ..
