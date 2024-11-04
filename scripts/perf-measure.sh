e670a06bae120475298a8812e474d3ce8278bb98
ansa <ansa@node-1.ansa-227881.ragger-pg0.wisc.cloudlab.us>
2024-11-04 15:23:41 -0600

    fix output name

--- scripts/perf-measure.sh
+++ scripts/perf-measure.sh
@@ -11,6 +11,6 @@ docker ps --no-trunc | tail -n +2 | while read -r line; do
 	name=$(echo $line | awk '{print $3}' | tr -cd '[:alnum:]')
 	echo "recording for service $name"
 	sudo perf stat -e cycles:u,cycles:k,instructions:u,instructions:k,resource_stalls.any,mem_load_retired.l1_miss,mem_load_retired.l2_miss --cgroup=system.slice/docker-"$process_id".scope -a sleep 60 > "$name"-1.data
-	sudo perf stat -e itlb_misses.stlb_hit,itlb_misses.miss_causes_a_walk,iTLB-load-misses,dTLB-load-misses,branch-misses --cgroup=system.slice/docker-"$process_id".scope -a sleep 60 > "$name"-1.data
+	sudo perf stat -e itlb_misses.stlb_hit,itlb_misses.miss_causes_a_walk,iTLB-load-misses,dTLB-load-misses,branch-misses --cgroup=system.slice/docker-"$process_id".scope -a sleep 60 > "$name"-2.data
 done
 cd ..
