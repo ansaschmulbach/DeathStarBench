75606f5ef7c477c5cac140d638ac79a5a0699f84
ansa <ansa@node-0.ansa-227881.ragger-pg0.wisc.cloudlab.us>
2024-11-04 14:51:54 -0600

    break up perf measurements

--- scripts/perf-measure.sh
+++ scripts/perf-measure.sh
@@ -2,6 +2,7 @@
 
 timestamp=$(date +"%Y%m%d_%H%M%S")
 output_dir="perfstat_output_$timestamp"
+echo 0 | sudo tee /proc/sys/kernel/nmi_watchdog
 mkdir "$output_dir"
 cd "$output_dir"
 echo "Writing results to $output_dir"
@@ -9,6 +10,7 @@ docker ps --no-trunc | tail -n +2 | while read -r line; do
 	process_id=$(echo $line | awk '{print $1}')
 	name=$(echo $line | awk '{print $3}' | tr -cd '[:alnum:]')
 	echo "recording for service $name"
-	sudo perf stat -e cycles:u,cycles:k,instructions:u,instructions:k,resource_stalls.any,mem_load_retired.l1_miss,mem_load_retired.l2_miss,itlb_misses.stlb_hit,itlb_misses.miss_causes_a_walk,iTLB-load-misses,dTLB-load-misses,branch-misses --cgroup=system.slice/docker-"$process_id".scope -a sleep 60 > "$name".data
+	sudo perf stat -e cycles:u,cycles:k,instructions:u,instructions:k,resource_stalls.any,mem_load_retired.l1_miss,mem_load_retired.l2_miss --cgroup=system.slice/docker-"$process_id".scope -a sleep 60 > "$name"-1.data
+	sudo perf stat -e itlb_misses.stlb_hit,itlb_misses.miss_causes_a_walk,iTLB-load-misses,dTLB-load-misses,branch-misses --cgroup=system.slice/docker-"$process_id".scope -a sleep 60 > "$name"-1.data
 done
 cd ..
