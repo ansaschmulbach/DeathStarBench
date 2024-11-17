4c76d2ef3cb187abff4a85a84f51fece27b73054
ansa <ansa@node-0.ansa-231691.ragger-pg0.wisc.cloudlab.us>
2024-11-17 17:22:04 -0600

    add my pintool

--- socialNetwork/src/pin-script.sh
+++ socialNetwork/src/pin-script.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 cd /social-network-microservices
 
-./pin/pin -t /social-network-microservices/sst-elements/src/sst/elements/prospero/tracetool/sstmemtrace.so -- $NAME &
+./pin/pin -t /social-network-microservices/sst-elements/src/sst/elements/prospero/tracetool/sstmemtrace.so -- $NAME 
 
-PID=$!
-sleep 10
-kill -SIGABRT $PID
