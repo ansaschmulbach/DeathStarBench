bfd61b2800efc981254a6975db7526163051f7bb
ansa <ansa@node-0.ansa-236795.ragger-pg0.wisc.cloudlab.us>
2024-12-17 12:09:11 -0600

    add to trace folder

--- socialNetwork/copy_all_files.sh
+++ socialNetwork/copy_all_files.sh
@@ -3,16 +3,21 @@
 # Get the list of all running Docker containers
 containers=$(docker ps --format '{{.Names}}')
 
+mkdir trace
+
 # Loop through each container
 for container in $containers; do
-  # Create a new folder with the name of the container
-  mkdir -p "$container"
-  
   # Get the list of files ending with trace_out or trace in the container
   files=$(docker exec "$container" sh -c 'ls /social-network-microservices/*trace_out /social-network-microservices/*trace 2>/dev/null')
   
-  # Loop through each file and copy it to the new folder
-  for file in $files; do
-    docker cp "$container":"$file" "$container"/
-  done
+  if [ -n "$files" ]; then
+    # Create a new folder with the name of the container
+    mkdir -p trace/"$container"
+
+    # Loop through each file and copy it to the new folder
+    for file in $files; do
+      docker cp "$container":"$file" trace/"$container"/
+    done
+  fi
+  
 done
