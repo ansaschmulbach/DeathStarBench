c44963a8a136a987e4bd0f03ecfc15587470b864
ansa <ansa@node-0.ansa-238420.ragger-pg0.wisc.cloudlab.us>
2025-01-16 02:35:47 -0600

    update to use custom pin

--- socialNetwork/quit-pin.sh
+++ socialNetwork/quit-pin.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
-container=d083bd4510bd
-pid=$(docker exec $container pgrep -f pin-script.sh)
-  
+image_names=("socialnetwork-social-graph-service" "socialnetwork-unique-id-service" "socialnetwork-url-shorten-service" "socialnetwork-home-timeline-service" "socialnetwork-user-timeline-service" "socialnetwork-user-service" "socialnetwork-user-mention-service" "socialnetwork-media-service" "socialnetwork-compose-post-service" "socialnetwork-text-service" "socialnetwork-post-storage-service")
 mkdir output
 cd output
-if [ -n "$pid" ]; then
-  # Kill the process
-  docker exec $container kill $pid
-  echo "Process $pid killed in container $container"
-	mkdir $container
-	cd $container
-	docker cp $container:/social-network-microservices/dmem_out .
-	docker cp $container:/social-network-microservices/imem_out .
-	cd ..
-else
-  echo "No matching process found in container $container"
-fi
+
+for image_name in "${image_names[@]}"; do
+	container=$(docker ps | grep $image_name | awk '{print $1}')
+	pid=$(docker exec $container pgrep -f pin-script.sh)
+  
+	if [ -n "$pid" ]; then
+	  # Kill the process
+	  docker exec $container kill $pid
+	  echo "Process $pid killed in container $container"
+		mkdir $container
+		cd $container
+		docker cp $container:/social-network-microservices/dmem_out .
+		docker cp $container:/social-network-microservices/imem_out .
+		cd ..
+	else
+	  echo "No matching process found in container $container"
+	fi
+done
--- socialNetwork/src/pin-script.sh
+++ socialNetwork/src/pin-script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /social-network-microservices
 
-./pin/pin -t /social-network-microservices/pin/source/tools/MyPinTool/obj-intel64/MyPinTool.so -- $NAME 
+./pin/pin -t /social-network-microservices/pin/source/tools/MyPinTool/obj-intel64/MyPinTool.so -o out -- $NAME 
 
